9dd30d86b5ddf17d9817ac7f0bebed42906b75e0
kura495 <104417866+kura495@users.noreply.github.com>
2023-05-31 23:45:13 +0900

    a

--- VectorCalc.cpp
+++ VectorCalc.cpp
@@ -8,8 +8,8 @@ Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
 }
 
 Vector3 Normalize(const Vector3& v1) { 
-	float Root = sqrt(v1.x * v1.x + v1.y * v1.y+v1.z+v1.z);
-	assert(Root != 0);
-	return Vector3(v1.x/Root,v1.y/Root,v1.z/Root);
+	float length = sqrt(v1.x * v1.x + v1.y * v1.y+v1.z+v1.z);
+	assert(length != 0);
+	return Vector3(v1.x/length,v1.y/length,v1.z/length);
 }
 
