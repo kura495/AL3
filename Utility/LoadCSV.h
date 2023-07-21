7d4f656639f6aa5e11a128176806a3e1492f3127
kura495 <104417866+kura495@users.noreply.github.com>
2023-07-21 20:26:12 +0900

    途中

---
+++ Utility/LoadCSV.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include<string>
+#include<sstream>
+#include<fstream>
+#include<cassert>
+/// <summary>
+	/// CSVを読み込む
+	/// </summary>
+	/// <param name="filename">ファイル名</param>
+	/// <returns></returns>
+std::stringstream LoadCSVData(const std::string& filename);
