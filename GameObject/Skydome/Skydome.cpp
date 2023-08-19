e5485416aa0a08181faf59e4f31ce94528609832
kura495 <104417866+kura495@users.noreply.github.com>
2023-08-19 16:50:10 +0900

    a

---
+++ GameObject/Skydome/Skydome.cpp
@@ -0,0 +1,20 @@
+#include "GameObject/Skydome/Skydome.h"
+
+void Skydome::Initialize(Model* model) { 
+	assert(model);
+	model_ = model;
+	worldTransform_.Initialize();
+	worldTransform_.scale_.x = 100.0f;
+	worldTransform_.scale_.y = 100.0f;
+	worldTransform_.scale_.z = 100.0f;
+	worldTransform_.UpdateMatrix();
+}
+
+void Skydome::Update() {
+
+
+}
+
+void Skydome::Draw(const ViewProjection viewProjection_) {
+	model_->Draw(worldTransform_, viewProjection_);
+}
---
+++ GameObject/Skydome/Skydome.h
@@ -0,0 +1,15 @@
+#pragma once
+#include"WorldTransform.h"
+#include"Model.h"
+#include<cassert>
+class Skydome {
+public:
+	void Initialize(Model* model);
+	void Update();
+	void Draw(const ViewProjection viewProjection_);
+
+private:
+	WorldTransform worldTransform_;
+	Model* model_ = nullptr;
+};
+
