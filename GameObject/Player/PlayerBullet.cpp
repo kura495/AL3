dca3713dceb8be8a80faba7bd515200ecc1b6f31
kura495 <104417866+kura495@users.noreply.github.com>
2023-08-28 19:47:10 +0900

    add PlayerBullet Function

--- GameObject/Player/PlayerBullet.cpp
+++ GameObject/Player/PlayerBullet.cpp
@@ -1,1 +1,54 @@
+﻿#include "PlayerBullet.h"
 #include "PlayerBullet.h"
+#include <cassert>
+
+void PlayerBullet::Initialize(Model* model, const Vector3& position, const Vector3& velocity) {
+	// modelチェック
+	assert(model);
+	// モデル読み込み
+	model_ = model;
+	// テクスチャ読み込み
+	textureHandle_ = TextureManager::Load("sample.png");
+	// プレイヤーの向きを玉にも反映
+	velocity_ = velocity;
+	// ワールドトランスフォーム初期化
+	worldTransform_.Initialize();
+	// 初期値セット
+	worldTransform_.translation_ = position;
+	SetcollitionAttribute(kCollitionAttributePlayer);
+	SetcollisionMask(~kCollitionAttributePlayer);
+}
+
+void PlayerBullet::Update() {
+
+	// 玉の移動
+	Move();
+
+	worldTransform_.UpdateMatrix();
+
+	// 時間経過でデスフラグを立てる
+	if (--deathTimer_ <= 0) {
+		isDead_ = true;
+	}
+}
+
+void PlayerBullet::Draw(const ViewProjection viewProjection_) {
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+}
+
+void PlayerBullet::OnCollision() { isDead_ = true; }
+
+Vector3 PlayerBullet::GetWorldPosition() {
+	Vector3 worldPos;
+	worldTransform_.UpdateMatrix();
+	worldPos.x = worldTransform_.matWorld_.m[3][0];
+	worldPos.y = worldTransform_.matWorld_.m[3][1];
+	worldPos.z = worldTransform_.matWorld_.m[3][2];
+	return worldPos;
+}
+
+// private関数
+void PlayerBullet::Move() {
+	// 加算
+	worldTransform_.translation_ = Add(worldTransform_.translation_, velocity_);
+}
--- GameObject/Player/PlayerBullet.h
+++ GameObject/Player/PlayerBullet.h
@@ -1,2 +1,41 @@
-#pragma once
-class PlayerBullet {};
+﻿#pragma once
+#include "Calc/Vector.h"
+#include "Model.h"
+#include "Utility/Collider.h"
+#include "Utility/CollisionConfig.h"
+#include "WorldTransform.h"
+class PlayerBullet : public Collider {
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model">モデル</param>
+	/// <param name="position">初期座標</param>
+	void Initialize(Model* model, const Vector3& position, const Vector3& velocity);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	void Draw(const ViewProjection viewProjection);
+
+	bool IsDead() const { return isDead_; };
+	void OnCollision();
+	Vector3 GetWorldPosition();
+
+private:
+	void Move();
+
+	Vector3 velocity_;
+	WorldTransform worldTransform_;
+	Model* model_ = nullptr;
+	uint32_t textureHandle_ = 0u;
+	// 玉の寿命
+	static const int32_t kLifeTime = 60 * 5;
+	// デスタイマー
+	int32_t deathTimer_ = kLifeTime;
+	// デスフラグ
+	bool isDead_ = false;
+};
+
